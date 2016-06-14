4c537220e6c42a60c0201c669b034f5ab4fbec69
Warren Parad <wparad@gmail.com>
2016-06-14 14:52:51 -0700

    Add ability to list and create instances.

--- README.md
+++ README.md
@@ -3,34 +3,44 @@ Free yourself from your Desktop, and reserve some cloud space just for yourself.
 
 [![npm version](https://badge.fury.io/js/cloudspace.svg)](https://badge.fury.io/js/cloudspace) [![Build Status](https://travis-ci.org/wparad/cloudspace.svg?branch=master)](https://travis-ci.org/wparad/cloudspace)
 
+## Prerequisites
+
+* Import a keypair named `Cloudspace-SSH`
+	* If necessary one can be created by using `ssh-keygen -b 4096 -t rsa`
+	* And then importing using the [AWS console UI](https://console.aws.amazon.com/ec2/v2/home#KeyPairs:sort=keyName).
+* Create an EC2 service Role (and [Service Profile](http://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_use_switch-role-ec2_instance-profiles.html)) called `Cloudspace`
+	* with the necessary permissions, to your AWS account (if necessary).
+* Create VPC Subnet (and VPC if necessary)
+	* Name the VPC Subnet `Cloudspace`
+	* Use ACL Restrictions for VPC to manage security:
+		* Inbound Rules
+
+			| Rule #            |        Type   |   Protocol    |   Port Range  | Source    | Allow / Deny |
+			| :---------------: |--------------:|--------------:|--------------:|----------:|-------------:|
+			| 100               | ALL Traffic   |   ALL         |   ALL         | Private IP| ALLOW        |
+
+		* Outbound Rules
+
+			| Rule #            |        Type   |   Protocol    |   Port Range  | Source    | Allow / Deny |
+			| :---------------: |--------------:|--------------:|--------------:|----------:|-------------:|
+			| 100               | ALL Traffic   |   ALL         |   ALL         | 0.0.0.0/0 | ALLOW        |
+
+		* Security Group
+			* Inbound Rules
+
+				|        Type   |   Protocol    |   Port Range  | Source    |
+				|--------------:|--------------:|--------------:|----------:|
+				| SSH (22)      |   TCP(6)      |   22          | 0.0.0.0/0 |
+			* Outbound Rules
+
+				|        Type   |   Protocol    |   Port Range  | Source    |
+				|--------------:|--------------:|--------------:|----------:|
+				| ALL Traffic   |   ALL         |   ALL         | 0.0.0.0/0 |
+
 ## Usage
 
 * `npm install -g cloudspace`
-* `cloudspace [-r --region=REGION]`
-
-## Use ACL Restrictions for VPC to manage security:
-
-* Inbound Rules
-
-	| Rule #            |        Type   |   Protocol    |   Port Range  | Source    | Allow / Deny |
-	| :---------------: |--------------:|--------------:|--------------:|----------:|-------------:|
-	| 100               | HTTPS (443)   |   TCP (6)     |   443         | 0.0.0.0/0 | ALLOW        |
-	| 110               | ALL Traffic   |   ALL         |   ALL         | Private IP| ALLOW        |
-
-* Outbound Rules
-
-	| Rule #            |        Type   |   Protocol    |   Port Range  | Source    | Allow / Deny |
-	| :---------------: |--------------:|--------------:|--------------:|----------:|-------------:|
-	| 110               | ALL Traffic   |   ALL         |   ALL         | 0.0.0.0/0 | ALLOW        |
-
-* Security Group
-	* Inbound Rules
-
-		|        Type   |   Protocol    |   Port Range  | Source    |
-		|--------------:|--------------:|--------------:|----------:|
-		| SSH (22)      |   TCP(6)      |   22          | 0.0.0.0/0 |
-	* Outbound Rules
-
-		|        Type   |   Protocol    |   Port Range  | Source    |
-		|--------------:|--------------:|--------------:|----------:|
-		| ALL Traffic   |   ALL         |   ALL         | 0.0.0.0/0 |
+* `cloudspace`
+	* `list`: Displays all EC2 instances
+	* `create`: Create an Cloudspace instance
+	* `terminate`: delete all instances
--- bin/cloudspace.js
+++ bin/cloudspace.js
@@ -5,21 +5,118 @@ const aws = require('aws-sdk');
 const commander = require('commander');
 const fs = require('fs');
 const path = require('path');
+const uuid = require('node-uuid');
 
 var packageMetadataFile = path.join(__dirname, '..', 'package.json');
 var package_metadata = require(packageMetadataFile);
 commander.version(package_metadata.version);
 
 //Set default region to test with
-aws.config.update({ region: 'us-east-1' });
+aws.config.update({
+	region: 'us-east-1',
+	apiVersions: {
+		ec2: '2015-10-01'
+	}
+});
+
+var ec2 = new aws.EC2();
+
+var ec2RegionProvider = () => {
+	return ec2.describeRegions({}).promise()
+	.then((data) => {
+		return Promise.all(data.Regions.map((r) => new aws.EC2({region: r.RegionName })));
+	});
+};
 
 commander
 	.command('create')
 	.description('Create the AWS Cloudspace')
 	.action(() => {
 		console.log('Creating your Cloudspace in AWS.');
-
+		ec2.runInstances({
+			ImageId: 'ami-f652979b',
+			InstanceType: 't2.micro',
+			MinCount: 1, MaxCount: 1,
+			KeyName: 'Cloudspace-SSH',
+			ClientToken: uuid.v4(),
+			/*
+			BlockDeviceMappings: [
+				{
+					DeviceName: '/dev/sda1',
+					Ebs: {
+						DeleteOnTermination: true,
+						Encrypted: false,
+						VolumeSize: 8,
+						VolumeType: 'standard'
+					}
+				}
+			],
+			*/
+			SubnetId: 'subnet-4ae0b512'
+		}).promise()
+		.then((result) => {
+			console.log(`Created Instance Id: ${result.Instances[0].InstanceId}`);
+			return ec2.createTags({
+				Resources: [result.Instances[0].InstanceId],
+				Tags: [{ Key: 'Name', Value: 'Cloudspace' }]
+			}).promise()
+			.catch((failure) => Promise.reject({
+				Error: 'Failed to udpated tags',
+				Detail: failure
+			}));
+		})
+		.catch((failure) => {
+			console.error(failure);
+		})
 		
+	});
+
+commander
+	.command('list')
+	.description('List the AWS Cloudspace')
+	.action(() => {
+		console.log('List your Cloudspace in AWS.');
+		ec2.describeRegions({}).promise()
+		.then((data) => {
+			return ec2RegionProvider().then((regionProviders) => {
+				return regionProviders.reduce((instancePromise, ec2Provider) => {
+					console.log(`Looking up instances in ${ec2Provider.config.region}`);
+					return ec2Provider.describeInstances({
+						Filters: [{Name: 'tag:Name', Values: ['Cloudspace']}]
+					}).promise()
+					.then((data) => {
+						var localInstances = [];
+						data.Reservations.map((reservations) => {
+							reservations.Instances.map((instance) => localInstances.push({
+								Id: instance.InstanceId,
+								Info: instance
+							}));
+						});
+						return instancePromise.then((instances) => instances.concat(localInstances));
+					});
+				}, Promise.resolve([]));
+			});
+		})
+		.then((instances) => {
+			console.log(JSON.stringify(instances, null, 2));
+		})
+		.catch((failure) => { console.log(failure); });
+	});
+
+commander
+	.command('terminate')
+	.description('Terminate the AWS Cloudspaces')
+	.action(() => {
+		var ec2 = new aws.EC2({apiVersion: '2015-10-01'});
+		ec2.describeInstances({
+			Filters: [{Name: 'tag:Role', Values: ['Cloudspace']}]
+		}, function(err, data) {
+			console.log("Destroying the following AWS EC2 instances: " + instances);
+			ec2.terminateInstances({InstanceIds: instances}, function(error, data) {
+				if (err) { console.log(err, err.stack); }
+				else { console.log(data); }
+			});
+		});
 	});
 
 commander.on('*', () => {
@@ -28,4 +125,4 @@ commander.on('*', () => {
 	commander.help();
 	process.exit(0);
 });
-commander.parse(process.argv[2] ? process.argv : process.argv.concat(['create']));
+commander.parse(process.argv[2] ? process.argv : process.argv.concat(['list']));
